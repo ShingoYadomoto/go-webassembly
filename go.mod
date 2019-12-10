e99425328848b2897f3d7d26bc0a0b798447ba8c
ShingoYadomoto <shingo.yadomoto@gmail.com>
2019-12-11 02:35:58 +0900

    fix

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ShingoYadomoto/anything-go/go-webassembly
+
+go 1.12
--- test-2.go
+++ test-2.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"syscall/js"
 	"math/rand"
+	"syscall/js"
 	"time"
 )
 
 var (
-	img = js.Global().Call("eval", "new Image()")
 	document = js.Global().Get("document")
+	img      = js.Global().Call("eval", "new Image()")
 )
 
 func main() {
 	img.Set("src", "./image/gopher.png")
 
 	body := document.Call("getElementsByTagName", "body").Index(0)
-	body.Call("addEventListener", "mousemove", js.FuncOf(func(this js.Value, args []js.Value) interface{}{
+	body.Call("addEventListener", "mousemove", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		render()
 		return nil
 	}))
@@ -23,16 +23,14 @@ func main() {
 	<-make(chan struct{}, 0)
 }
 
-
-func render(){
+func render() {
 	canvas := document.Call("getElementById", "sample")
 	ctx := canvas.Call("getContext", "2d")
 
 	rand.Seed(time.Now().UnixNano())
 	x := rand.Intn(1000)
-	rand.Seed(time.Now().UnixNano()+100000)
-	y:= rand.Intn(1000)
+	rand.Seed(time.Now().UnixNano() + 100000)
+	y := rand.Intn(1000)
 
-	ctx.Call("drawImage", img, x,y)
+	ctx.Call("drawImage", img, x, y)
 }
-
